de548b9705296cc6682a56d1268b28feb165d65c
DANIEL Nicolas <NDL1@dsone.3ds.com>
2020-05-02 12:23:18 +0200

    remove unused font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
 // custom typefaces
 import "typeface-montserrat"
-import "typeface-merriweather"
-
 import "prismjs/themes/prism.css"
+
+export function replaceHydrateFunction() {
+    return (element, container, callback) => {
+        ReactDOM.render(element, container, callback);
+    };
+}
