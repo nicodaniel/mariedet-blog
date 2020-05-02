1b15c7e2abcfccbee07a22f35c40c279a2b1167e
DANIEL Nicolas <NDL1@dsone.3ds.com>
2020-05-02 21:50:01 +0200

    fix path to img in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const NotFoundPage = ({ data, location }) => {
                 <div style={{fontSize: '100px', fontWeight: 700}}>D'OH!</div>
                 <div>This is not the route you are looking for ...</div>
             </div>
-            <div><img src={"homer-404.jpg"} height={400} alt="404-error" /></div>
+            <div><img src={"../../homer-404.jpg"} height={400} alt="404-error" /></div>
         </div>
     </Layout>
   )
