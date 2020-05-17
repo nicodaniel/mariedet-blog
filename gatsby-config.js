8c066d48b387e82e1bced5f49f38c346d1fb0e3c
NDL1 <nicolas.daniel@3ds.com>
2020-05-17 21:10:16 +0200

    update image width of articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,8 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            maxWidth: 590,
+                            //maxWidth: 590,
+                            maxWidth: 1000,
                         },
                     },
                     {
